4c02b8515360a96b6a8631712ca259379d57a6a1
unknown <you@example.com>
2025-01-03 22:37:12 -0300

    primera subida..

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using Internal;
+using System;
+
+namespace ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("hola sebi");
+        }
+    }
+}
